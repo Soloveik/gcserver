774aa5f28802468dec14c8096d6e1842f8674502
Soloveik <Soloveik@mail.ru>
2016-11-06 20:59:53 +0300

    Xif

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,7 @@ set :unicorn_pid,    "/var/run/unicorn/#{fetch(:user)}/" \
 set :bundle_without, %w{development test}.join(' ')             # this is default
 set :use_sudo,       false
 
-set :repo_url,       "#{fetch(:user)}@phosphorus.locum.ru:" \
-                     "git/#{fetch(:application)}.git"
+set :repo_url,       "git@github.com:Nerrey/gcserver.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
