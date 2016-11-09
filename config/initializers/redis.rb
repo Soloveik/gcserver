85b92c3fc18b659160cb18259506bbb62ec08ba8
Soloveik <Soloveik@mail.ru>
2016-11-09 22:22:46 +0300

    socketfix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  $redis = Redis.new(db: 0, path: '/tmp/gcredis.sock')
+  $redis = Redis.new(db: 0, path: '/tmp/redis.sock')
 else
   $redis = Redis.new(db: 0)#, path: '/tmp/redis.sock')
 end
