ae7575a8330feb383feed63054947588f7317fe4
Nerrey <kolud@bk.ru>
2016-12-05 20:24:38 +0300

    fix db.yml path

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Gcserver
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     if Rails.env == "production"
-      self.paths['config/database'] = '../shared/database.yml'
+      self.paths['config/database'] = '../../shared/database.yml'
     end
   end
 end
